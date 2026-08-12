4dff716144ee4bbef936157fbec5b4dea4fb4332
kushal <kushala033@hmail.com>
2026-08-12 12:40:12 +0530

    new java file

---
+++ hellowprld.java
@@ -0,0 +1,7 @@
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins!");
+        System.out.println("Build Success!");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+    }
+}
